71eea92bb91ae4de21950c1a734d96e33cc9a543
Cabangon.252762 <Cabangon.252762@LAB-C-PC23>
2023-10-16 18:53:50 +0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart thread = new ThreadStart(MyThreadClass.Thread1);
+            Thread threadA = new Thread(thread);
+            Thread threadB = new Thread(thread);
+
+            threadA.Name = "ThreadA process:";
+            threadB.Name = "ThreadB process:";
+
+            Console.WriteLine(label1.Text.ToString());
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+            label1.Text = "End of a Thread";
+            Console.WriteLine(label1.Text.ToString());
+        }
+    }
+}
---
+++ WindowsFormsApp1/MyThreadClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int x = 0; x < 6; x++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + x);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
+
